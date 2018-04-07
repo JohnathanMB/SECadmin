756230a0885703db5650b09c6c057e93dbe322ba
unknown <johnathan.monsalve@udea.edu.co>
2018-04-07 01:00:20 -0500

    Resolviendo problemas

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -19,15 +19,19 @@ export class RestProvider {
   //apiUrlRegistrar = 'http://api-rest-sec.herokuapp.com/u/signup';
   apiUrlRegistrarAdmin = 'http://api-rest-sec.herokuapp.com/e/administrador';
   apiUrlRegistrarRepartidor ='http://api-rest-sec.herokuapp.com/e/repartidor';
-  apiUrLoginEmpleado= 'http://api-rest-sec.herokuapp.com/e/repartidor';
-  apiUrlGetMedicamentos = 'http://api-rest-sec.herokuapp.com/e/repartidor';
-  apiUrlGetMedicamentoCategoria = 'http://api-rest-sec.herokuapp.com/e/repartidor';
-  apiUrlPostMedicamento= 'http://api-rest-sec.herokuapp.com/m/medicamentos/';
-  apiUrlModificarMedicamento = 'http://api-rest-sec.herokuapp.com/m/medicamentos/:id';
-  apiUrlEliminarMedicamento = 'http://api-rest-sec.herokuapp.com/m/medicamentos/:id';
+  apiUrLoginEmpleado= 'http://api-rest-sec.herokuapp.com/e/login';
+  apiUrlGetMedicamentos = 'http://api-rest-sec.herokuapp.com/m/medicamentos';
+  apiUrlGetMedicamentoCategoria = 'http://api-rest-sec.herokuapp.com/m/medicamentos/:categoria';
+  apiUrlPostMedicamento= 'http://api-rest-sec.herokuapp.com/m/medicamentos';
+  apiUrlModificarMedicamento = 'http://api-rest-sec.herokuapp.com/m/put/medicamentos/:id';
+  apiUrlEliminarMedicamento = 'http://api-rest-sec.herokuapp.com/m/del/medicamentos/:id';
 
 
   addMedicamento(data){
+    //console.log(data);
+    
+
+    console.log(JSON.stringify(data));
     return new Promise((resolve, reject) => {
       this.http.post(this.apiUrlPostMedicamento, JSON.stringify(data))
         .subscribe(res => {
